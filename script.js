b26bb968e4f78834503c44b46aecee9c7aa82a08
penguin4life21 <69492010+penguin4life21@users.noreply.github.com>
2021-08-31 09:16:45 +1000

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
+cash = 0
+cash_per_second = 0
+cash_per_click = 1
 
+function increment() {
+  cash = cash + cash_per_second
+  document.getElementById("cash_display").innerText = cash
+}
+
+function click() {
+  cash = cash + cash_per_click
+  document.getElementById("cash_display").innerText = cash
+}
+
+setInterval(increment, 1000)
