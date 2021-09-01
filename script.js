93342f28fcdbdcbc01e494642d8d335af3eda09e
penguin4life21 <69492010+penguin4life21@users.noreply.github.com>
2021-09-02 08:59:03 +1000

    Update script.js

--- script.js
+++ script.js
@@ -5,8 +5,13 @@ t1_gens = 0
 t1_income = 1
 t1_baseprice = 25
 t1_price_multi = 1.01
+t2_gens = 0
+t2_income = 10
+t2_baseprice = 100
+t2_price_multi = 1.02
 
-function buy_gen() {
+
+function buy_t1gen() {
   if (cash >= Math.round(t1_baseprice * (t1_price_multi ** t1_gens))) {
     cash -= Math.round(t1_baseprice * (t1_price_multi ** t1_gens))
     cash_per_second += t1_income
@@ -18,10 +23,23 @@ function buy_gen() {
   }
 }
 
+function buy_t1gen() {
+  if (cash >= Math.round(t2_baseprice * (t2_price_multi ** t2_gens))) {
+    cash -= Math.round(t2_baseprice * (t2_price_multi ** t2_gens))
+    cash_per_second += t2_income
+    t2_gens += 1
+    document.getElementById("t2_price").innerText = Math.round(t2_baseprice * (t2_price_multi ** t2_gens))
+    update_display()
+  } else {
+    alert("ERROR, Not Enough Cash") 
+  }
+}
+
 function update_display() {
    document.getElementById("cash_display").innerText = cash
    document.getElementById("cash_per_second_display").innerText = cash_per_second
    document.getElementById("t1_gens_owned").innerText = t1_gens
+   document.getElementById("t2_gens_owned").innerText = t2_gens
 }
 
 function increment() {
